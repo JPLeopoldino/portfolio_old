d0837712566b63958560b8133cbe1e54cf108fa3
João Pedro <leopoldinodev@gmail.com>
2023-07-19 15:56:19 -0300

    refactor: pass the correct props to the svg elements

--- src/assets/github.tsx
+++ src/assets/github.tsx
@@ -9,9 +9,9 @@ export default function GithubIcon(props: SVGAttributes<SVGSVGElement>) {
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       className="feather feather-github"
       {...props}
     >
