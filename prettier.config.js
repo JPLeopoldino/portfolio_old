aad6aee17b29c889f5e33e6fc3371be6ff351fa6
João Pedro <leopoldinodev@gmail.com>
2023-08-09 16:32:05 -0300

    chore: add and configure prettier and prettier-plugin-tailwindcss

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+/** @type {import("prettier").Options} */
+const config = {
+  trailingComma: "es5",
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  plugins: ["prettier-plugin-tailwindcss"],
+  tailwindConfig: "./tailwind.config.js",
+};
+
+module.exports = config;
