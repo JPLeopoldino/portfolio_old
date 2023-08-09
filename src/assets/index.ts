20ea41efa7762bdadd77933c914b8139f89cc706
João Pedro <leopoldinodev@gmail.com>
2023-08-09 19:11:14 -0300

    feat: remove unused icons

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,9 +1,1 @@
-export { default as CopyIcon } from './copy';
-export { default as FileIcon } from './file';
-export { default as GithubIcon } from './github';
-export { default as LinkedinIcon } from './linkedin';
-export { default as MailIcon } from './mail';
-export { default as MenuIcon } from './menu';
-export { default as MoonIcon } from './moon';
 export { default as profileImg } from './profile-img.jpg';
-export { default as SunIcon } from './sun';
